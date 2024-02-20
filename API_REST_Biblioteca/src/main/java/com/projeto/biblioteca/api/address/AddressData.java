d75986ac40b1a89fbdb6bced92c08ef40925ab62
CauaConceicao20 <CauaConceicaoDev@hotmail.com>
2024-02-19 22:29:15 -0300

    Adicionando Anotações de validação ao AddressData.

--- API_REST_Biblioteca/src/main/java/com/projeto/biblioteca/api/address/AddressData.java
+++ API_REST_Biblioteca/src/main/java/com/projeto/biblioteca/api/address/AddressData.java
@@ -1,9 +1,20 @@
 package com.projeto.biblioteca.api.address;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+
 public record AddressData(
+		
+		@NotBlank
+		@Pattern(regexp ="\\d{8}")
 		String cep,
+		
+		@NotBlank
 		String neighborhood,
+		
+		@NotBlank
 		String city,
+		
 		String complement,
 		String number) {
 
